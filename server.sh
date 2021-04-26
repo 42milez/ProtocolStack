20866e2b04ac187c81f02cf98a56dfd6410a69aa
Akihiro TAKASE <42milez@gmail.com>
2021-04-26 20:31:20 +0900

    update

--- server.sh
+++ server.sh
@@ -6,20 +6,27 @@ readonly CMD=$1
 readonly VM_NAME=ps.vagrant
 
 start() {
-  if ! vagrant status ${VM_NAME} | grep "running (virtualbox)" > /dev/null 2>&1; then
+  if vagrant status ${VM_NAME} | grep "running (virtualbox)" > /dev/null 2>&1; then
     echo "already started."
     exit 1
   fi
   vagrant up
+
+  if [ -e mutagen.yml.lock ]; then
+    mutagen project terminate
+  fi
   mutagen project start -f mutagen.yml
 }
 
 stop() {
-  if ! vagrant status ${VM_NAME} | grep "poweroff (virtualbox)" > /dev/null 2>&1; then
+  if [ -e mutagen.yml.lock ]; then
+    mutagen project terminate
+  fi
+
+  if vagrant status ${VM_NAME} | grep "poweroff (virtualbox)" > /dev/null 2>&1; then
     echo "already stopped."
     exit 1
   fi
-  mutagen project terminate
   vagrant halt
 }
 
