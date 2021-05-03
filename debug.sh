ba46170dfb2af7f83d7111b5795393a562e31ae4
Akihiro TAKASE <42milez@gmail.com>
2021-05-03 15:38:41 +0900

    update

--- debug.sh
+++ debug.sh
@@ -2,6 +2,11 @@
 
 set -eu
 
+if [ $# -eq 0 ]; then
+  ./debug.sh help
+  exit 1
+fi
+
 readonly CMD=$1
 readonly DELVE="dlv --listen=:2345 --headless=true --api-version=2 --accept-multiclient exec"
 readonly TCP_CLIENT=./bin/tcp_client
@@ -10,6 +15,16 @@ readonly TCP_SERVER=./bin/tcp_server
 case $CMD in
   "client") $DELVE $TCP_CLIENT;;
   "server") $DELVE $TCP_SERVER;;
+  "help") {
+    echo "Usage:"
+    echo ""
+    echo "  ./debug.sh <command>"
+    echo ""
+    echo "The commands are:"
+    echo ""
+    echo "  client   run the TCP client, and begin a debug session"
+    echo "  server   run the TCP server, and begin a debug session"
+  };;
   *) {
     echo "Command not supported."
     exit 1
