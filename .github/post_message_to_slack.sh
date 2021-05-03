bdc97a1d2e68002ebc5909515e2a22af908435c6
Akihiro TAKASE <42milez@gmail.com>
2021-05-04 01:45:45 +0900

    update

---
+++ .github/post_message_to_slack.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -eu
+
+readonly DATA=$(cat <<EOF
+{
+  "attachments": [
+    {
+      "blocks": [
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": "${WORKFLOW_NAME} <https://github.com/${REPOSITORY_OWNER}/${REPOSITORY_NAME}/actions/runs/${WORKFLOW_RUN_ID}|#${WORKFLOW_RUN_NUMBER} (${WORKFLOW_RUN_ID})> of ${REPOSITORY_OWNER}/${REPOSITORY_NAME}@${REPOSITORY_REF} by ${GITHUB_ACTOR} failed."
+          }
+        }
+      ],
+      "color": "danger",
+    }
+  ]
+}
+EOF
+)
+
+curl -X POST \
+     -H "Content-type: application/json; charset=utf-8" \
+     -H "Authorization: Bearer ${SLACK_BOT_USER_OAUTH_TOKEN}" \
+     -d "${DATA}" \
+     https://slack.com/api/chat.postMessage
