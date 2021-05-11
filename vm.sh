02027ba5ae1ff4a001929a4cf4ae4bc1a57e205c
Akihiro TAKASE <42milez@gmail.com>
2021-05-11 15:49:01 +0900

    Revert "update"
    This reverts commit f4741a8fe9739a30aa3b12b56f82dbd1875de298.

--- vm.sh
+++ vm.sh
@@ -13,8 +13,8 @@ readonly HOSTNAME=$(vagrant status | awk 'NR==3,NR==3 {print $1}')
 readonly IS_INITIALIZED=$(if [ -e "${WORK_DIR}/.vagrant/machines/${HOSTNAME}/virtualbox/id" ]; then echo true; else echo false; fi)
 
 readonly CMD=$1
-#readonly MUTAGEN_FILE=mutagen.yml
-#readonly MUTAGEN_LOCK_FILE=mutagen.yml.lock
+readonly MUTAGEN_FILE=mutagen.yml
+readonly MUTAGEN_LOCK_FILE=mutagen.yml.lock
 readonly VM_NAME=ps.vagrant
 
 start() {
@@ -31,8 +31,8 @@ start() {
     vagrant reload
   fi
 
-  #test -e "${MUTAGEN_LOCK_FILE}" && mutagen project terminate
-  #mutagen project start -f "${MUTAGEN_FILE}"
+  test -e "${MUTAGEN_LOCK_FILE}" && mutagen project terminate
+  mutagen project start -f "${MUTAGEN_FILE}"
 
   echo "Virtual machine has started. ✨"
 }
@@ -40,7 +40,7 @@ start() {
 stop() {
   echo "Shutting down virtual machine..."
 
-  #test -e "${MUTAGEN_LOCK_FILE}" && mutagen project terminate
+  test -e "${MUTAGEN_LOCK_FILE}" && mutagen project terminate
 
   if vagrant status ${VM_NAME} | grep "poweroff (virtualbox)" > /dev/null 2>&1; then
     echo "Virtual machine is already stopped."
@@ -60,7 +60,7 @@ restart() {
   fi
   vagrant reload
 
-  #mutagen project reset -f "${MUTAGEN_FILE}"
+  mutagen project reset -f "${MUTAGEN_FILE}"
 
   echo "Virtual machine has restarted. 👍"
 }
@@ -76,12 +76,9 @@ case $CMD in
     echo ""
     echo "The commands are:"
     echo ""
-    #echo "  start     start a VM and create a Mutagen session"
-    #echo "  stop      stop the VM and terminate the Mutagen session"
-    #echo "  restart   restart the VM and recreate a Mutagen session"
-    echo "  start     start a VM"
-    echo "  stop      stop the VM"
-    echo "  restart   restart the VM"
+    echo "  start     start a VM and create a Mutagen session"
+    echo "  stop      stop the VM and terminate the Mutagen session"
+    echo "  restart   restart the VM and recreate a Mutagen session"
   };;
   *) {
     echo "Command not supported."
