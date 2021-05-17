eb2930403d6b3ce354d99ee7f84510b8c7782620
Akihiro TAKASE <42milez@gmail.com>
2021-05-17 16:07:45 +0900

    clean up

--- src/ethernet/ethernet.go
+++ src/ethernet/ethernet.go
@@ -1,6 +1,7 @@
 package ethernet
 
 import (
+	"fmt"
 	e "github.com/42milez/ProtocolStack/src/error"
 	l "github.com/42milez/ProtocolStack/src/log"
 	"syscall"
@@ -23,23 +24,20 @@ var EthAddrBroadcast = MAC("FF:FF:FF:FF:FF:FF")
 
 var endian int
 
-type EthHeader struct {
-	Dst  [EthAddrLen]byte
-	Src  [EthAddrLen]byte
-	Type uint16
+type EthAddr [EthAddrLen]byte
+
+func (v EthAddr) Equal(vv []byte) bool {
+	return v[0] != vv[0] && v[1] != vv[1] && v[2] != vv[2] && v[3] != vv[3] && v[4] != vv[4] && v[5] != vv[5]
 }
 
-func (h EthHeader) EqualDstAddr(v []byte) bool {
-	return h.Dst[0] != v[0] &&
-		h.Dst[1] != v[1] &&
-		h.Dst[2] != v[2] &&
-		h.Dst[3] != v[3] &&
-		h.Dst[4] != v[4] &&
-		h.Dst[5] != v[5]
+func (v EthAddr) String() string {
+	return fmt.Sprintf("%v:%v:%v:%v:%v:%v", v[0], v[1], v[2], v[3], v[4], v[5])
 }
 
-func (h EthHeader) TypeAsString() string {
-	switch ntoh16(h.Type) {
+type EthType uint16
+
+func (v EthType) String() string {
+	switch ntoh16(uint16(v)) {
 	case EthTypeArp:
 		return "ARP"
 	case EthTypeIpv4:
@@ -47,14 +45,20 @@ func (h EthHeader) TypeAsString() string {
 	case EthTypeIpv6:
 		return "IPv6"
 	default:
-		return "Unknown Type"
+		return "UNKNOWN"
 	}
 }
 
+type EthHeader struct {
+	Dst  EthAddr
+	Src  EthAddr
+	Type EthType
+}
+
 func EtherDump(hdr *EthHeader) {
-	l.I("\tmac (dst):   %d:%d:%d:%d:%d:%d", hdr.Dst[0], hdr.Dst[1], hdr.Dst[2], hdr.Dst[3], hdr.Dst[4], hdr.Dst[5])
-	l.I("\tmac (src):   %d:%d:%d:%d:%d:%d", hdr.Src[0], hdr.Src[1], hdr.Src[2], hdr.Src[3], hdr.Src[4], hdr.Src[5])
-	l.I("\tethertype: 0x%04x (%s)", ntoh16(hdr.Type), hdr.TypeAsString())
+	l.I("\tmac (dst): %d:%d:%d:%d:%d:%d", hdr.Dst[0], hdr.Dst[1], hdr.Dst[2], hdr.Dst[3], hdr.Dst[4], hdr.Dst[5])
+	l.I("\tmac (src): %d:%d:%d:%d:%d:%d", hdr.Src[0], hdr.Src[1], hdr.Src[2], hdr.Src[3], hdr.Src[4], hdr.Src[5])
+	l.I("\teth_type:  0x%04x (%s)", hdr.Type.String(), hdr.Type.String())
 }
 
 func ReadFrame(dev *Device) e.Error {
@@ -77,8 +81,8 @@ func ReadFrame(dev *Device) e.Error {
 	}
 
 	hdr := (*EthHeader)(unsafe.Pointer(&buf))
-	if !hdr.EqualDstAddr(dev.Addr.Byte()) {
-		if !hdr.EqualDstAddr(EthAddrBroadcast.Byte()) {
+	if !hdr.Dst.Equal(dev.Addr.Byte()) {
+		if !hdr.Dst.Equal(EthAddrBroadcast.Byte()) {
 			return e.Error{Code: e.NoDataToRead}
 		}
 	}
@@ -88,7 +92,7 @@ func ReadFrame(dev *Device) e.Error {
 	EtherDump(hdr)
 
 	l.I("\tdevice:       %v (%v) ", dev.Name, dev.Priv.Name)
-	l.I("\tethertype:    %v (0x%04x) ", hdr.TypeAsString(), ntoh16(hdr.Type))
+	l.I("\teth_type:     %v (0x%04x) ", hdr.Type.String(), hdr.Type)
 	l.I("\tframe length: %v ", flen)
 
 	return e.Error{Code: e.OK}
--- src/ethernet/ethernet_test.go
+++ src/ethernet/ethernet_test.go
@@ -1,26 +1,35 @@
 package ethernet
 
-import "testing"
+import (
+	"testing"
+)
 
-func TestEthHeader_TypeAsString(t *testing.T) {
-	h := EthHeader{Type: 0x0608}
+func TestEthType_String(t *testing.T) {
+	ethType := EthType(0x0608)
 	want := "ARP"
-	got := h.TypeAsString()
+	got := ethType.String()
 	if got != want {
-		t.Errorf("EthHeader.TypeAsString() = %v; want %v", got, want)
+		t.Errorf("EthType.String() = %v; want %v", got, want)
 	}
 
-	h = EthHeader{Type: 0x0008}
+	ethType = EthType(0x0008)
 	want = "IPv4"
-	got = h.TypeAsString()
+	got = ethType.String()
 	if got != want {
-		t.Errorf("EthHeader.TypeAsString() = %v; want %v", got, want)
+		t.Errorf("EthType.String() = %v; want %v", got, want)
 	}
 
-	h = EthHeader{Type: 0xdd86}
+	ethType = EthType(0xdd86)
 	want = "IPv6"
-	got = h.TypeAsString()
+	got = ethType.String()
 	if got != want {
-		t.Errorf("EthHeader.TypeAsString() = %v; want %v", got, want)
+		t.Errorf("EthType.String() = %v; want %v", got, want)
+	}
+
+	ethType = EthType(0x0000)
+	want = "UNKNOWN"
+	got = ethType.String()
+	if got != want {
+		t.Errorf("EthType.String() = %v; want %v", got, want)
 	}
 }
