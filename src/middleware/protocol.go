c28bff8ea128a5f801b90d8871fa57c9c8c71fd0
Akihiro TAKASE <42milez@gmail.com>
2021-05-23 13:56:17 +0900

    update

--- src/middleware/protocol.go
+++ src/middleware/protocol.go
@@ -29,16 +29,6 @@ func (t ProtocolType) String() string {
 	}
 }
 
-// TODO: delete this comment later
-//struct net_protocol {
-//    struct net_protocol *next;
-//    char name[16];
-//    uint16_t type;
-//    pthread_mutex_t mutex;   // mutex for input queue
-//    struct queue_head queue; // input queue
-//    void (*handler)(const uint8_t *data, size_t len, struct net_device *dev);
-//};
-
 type Protocol struct {
 	Type    ProtocolType
 	Mutex   *sync.Mutex
