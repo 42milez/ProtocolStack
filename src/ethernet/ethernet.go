97cbb41ac9ccc921a389f15992a99c37e4f44c67
Akihiro TAKASE <42milez@gmail.com>
2021-05-26 18:20:00 +0900

    clean up

--- src/ethernet/ethernet.go
+++ src/ethernet/ethernet.go
@@ -23,8 +23,6 @@ const EthTypeIpv6 EthType = 0x86dd
 var EthAddrAny = EthAddr{0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
 var EthAddrBroadcast = EthAddr{0xff, 0xff, 0xff, 0xff, 0xff, 0xff}
 
-//var endian int
-
 type EthAddr [EthAddrLen]byte
 
 func (v EthAddr) Equal(vv EthAddr) bool {
@@ -100,15 +98,3 @@ func ReadFrame(fd int, addr EthAddr, sc psSyscall.ISyscall) psErr.Error {
 
 	return psErr.Error{Code: psErr.OK}
 }
-
-//func ntoh16(n uint16) uint16 {
-//	if endian == psBinary.LittleEndian {
-//		return swap16(n)
-//	} else {
-//		return n
-//	}
-//}
-
-//func swap16(v uint16) uint16 {
-//	return (v << 8) | (v >> 8)
-//}
