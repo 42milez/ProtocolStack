050e02fa3cd1da948d5b9878a657421939384dba
Akihiro TAKASE <42milez@gmail.com>
2021-06-03 15:40:56 +0900

    update

--- src/binary/binary.go
+++ src/binary/binary.go
@@ -11,6 +11,10 @@ func Swap16(v uint16) uint16 {
 	return (v << 8) | (v >> 8)
 }
 
+func Swap32(v uint32) uint32 {
+	return (v&0xff000000)>>24 | (v&0x00ff0000)>>8 | (v&0x0000ff00)<<8 | (v&0x000000ff)<<24
+}
+
 func byteOrder() goBinary.ByteOrder {
 	x := 0x0100
 	p := unsafe.Pointer(&x)
