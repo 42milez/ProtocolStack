e64c20d9f6fb98ed34fba35d176bd0bda1b56e1b
Akihiro TAKASE <42milez@gmail.com>
2021-06-07 21:34:20 +0900

    update

--- src/cli/ping.go
+++ src/cli/ping.go
@@ -5,7 +5,10 @@ import (
 	"fmt"
 	psErr "github.com/42milez/ProtocolStack/src/error"
 	psLog "github.com/42milez/ProtocolStack/src/log"
+	"github.com/42milez/ProtocolStack/src/mw"
+	"github.com/42milez/ProtocolStack/src/net/icmp"
 	"github.com/spf13/cobra"
+	"syscall"
 )
 
 var count uint32
@@ -25,8 +28,34 @@ var pingCmd = &cobra.Command{
 		if err := setup(); err != psErr.OK {
 			psLog.F("initialization failed")
 		}
-		fmt.Printf("send icmp request to %s\n", dst)
+		send()
+		for {
+			sig := <-sigCh
+			psLog.I(fmt.Sprintf("signal: %s", sig))
+			if sig == syscall.SIGINT || sig == syscall.SIGTERM {
+				stopServices()
+				break
+			}
+		}
 	},
+}
+
+func send()  {
+	id := 0
+	seq := 0
+	msg := &mw.IcmpTxMessage{
+		Type: icmp.Echo,
+		Code: 0,
+		Content: uint32(id)<<16 | uint32(seq),
+		Payload: []byte{1,2,3,4},
+		Src: mw.IP{0, 0, 0, 0},
+		Dst: mw.ParseIP(dst),
+	}
+	mw.IcmpTxCh <- msg
+}
+
+func receive() {
+
 }
 
 func init() {
