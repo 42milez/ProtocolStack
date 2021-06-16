24deadb6d3729cf74d2a889ede4d6d84d2198aa3
Akihiro TAKASE <42milez@gmail.com>
2021-06-16 12:05:24 +0900

    update

--- src/net/ip/ip_test.go
+++ src/net/ip/ip_test.go
@@ -1,1 +1,11 @@
 package ip
+
+import "testing"
+
+func TestPacketID_Next(t *testing.T) {
+	want := uint16(0)
+	got := id.Next()
+	if got != want {
+		t.Errorf("PacketID.Next() = %d; want %d", got, want)
+	}
+}
