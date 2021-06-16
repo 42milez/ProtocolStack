e01cdf709fc3e4c1075e4300cde4f684cebc2ba9
Akihiro TAKASE <42milez@gmail.com>
2021-06-16 12:49:07 +0900

    update

--- src/net/ip/ip_test.go
+++ src/net/ip/ip_test.go
@@ -1,7 +1,13 @@
 package ip
 
 import (
+	"bytes"
+	"encoding/binary"
+	psErr "github.com/42milez/ProtocolStack/src/error"
 	psLog "github.com/42milez/ProtocolStack/src/log"
+	"github.com/42milez/ProtocolStack/src/mw"
+	"github.com/42milez/ProtocolStack/src/net/eth"
+	"github.com/42milez/ProtocolStack/src/repo"
 	"github.com/42milez/ProtocolStack/src/worker"
 	"github.com/golang/mock/gomock"
 	"sync"
@@ -13,6 +19,23 @@ func TestPacketID_Next(t *testing.T) {
 	got := id.Next()
 	if got != want {
 		t.Errorf("PacketID.Next() = %d; want %d", got, want)
+	}
+}
+
+func TestReceive(t *testing.T) {
+	_, teardown := setupIpTest(t)
+	defer teardown()
+
+	dev := createValidTapDevice()
+	iface := createValidIface()
+	_ = repo.IfaceRepo.Register(iface, dev)
+	packet := createValidIpPacket()
+
+	want := psErr.OK
+	got := Receive(packet, dev)
+
+	if got != want {
+		t.Errorf("Receive() = %s; want %s", got, want)
 	}
 }
 
@@ -44,6 +67,55 @@ func TestStop(t *testing.T) {
 
 	if rcvMonMsg.Current != worker.Stopped || sndMonMsg.Current != worker.Stopped {
 		t.Errorf("Stop() failed")
+	}
+}
+
+func createValidIface() *mw.Iface {
+	return &mw.Iface{
+		Family:    mw.V4AddrFamily,
+		Unicast:   mw.IP{192, 168, 0, 1},
+		Netmask:   mw.IP{255, 255, 255, 0},
+		Broadcast: mw.IP{192, 168, 0, 255},
+	}
+}
+
+func createValidIpPacket() []byte {
+	payload := []byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	hdr := &mw.IpHdr{}
+	hdr.VHL = uint8(ipv4<<4) | uint8(HdrLenMin/4)
+	hdr.TotalLen = uint16(HdrLenMin + len(payload))
+	hdr.ID = 0
+	hdr.TTL = 0xff
+	hdr.Protocol = ICMP
+	hdr.Src = mw.V4Addr{192, 168, 0, 1}
+	hdr.Dst = mw.V4Addr{192, 168, 1, 1}
+
+	buf := new(bytes.Buffer)
+	if err := binary.Write(buf, binary.BigEndian, hdr); err != nil {
+		return nil
+	}
+	if err := binary.Write(buf, binary.BigEndian, &payload); err != nil {
+		return nil
+	}
+	packet := buf.Bytes()
+
+	csum := mw.Checksum(packet, 0)
+	packet[10] = uint8((csum & 0xff00) >> 8)
+	packet[11] = uint8(csum & 0x00ff)
+
+	return packet
+}
+
+func createValidTapDevice() *eth.TapDevice {
+	return &eth.TapDevice{
+		Device: mw.Device{
+			Type_: mw.EthernetDevice,
+			Name_: "net0",
+			MTU_:  mw.EthPayloadLenMax,
+			Flag_: mw.BroadcastFlag | mw.NeedArpFlag,
+			Addr_: mw.EthAddr{11, 12, 13, 14, 15, 16},
+			Priv_: mw.Privilege{FD: 3, Name: "tap0"},
+		},
 	}
 }
 
