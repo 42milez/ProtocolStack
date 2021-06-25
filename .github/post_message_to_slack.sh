13e474549cbf11b35b48353403cee9ccad613460
Akihiro TAKASE <42milez@gmail.com>
2021-06-25 11:09:20 +0900

    update

--- .github/post_message_to_slack.sh
+++ .github/post_message_to_slack.sh
@@ -17,11 +17,11 @@ readonly STATUS_MESSAGE
 readonly COMMIT_HASH=$(echo "${GITHUB_SHA}" | cut -c 1-7)
 
 readonly LINK=$(
-if [[ "${GITHUB_REPOSITORY_REF}" =~ ^"refs/heads/" ]]; then
-  readonly BRANCH=$(echo "${GITHUB_REPOSITORY_REF}" | cut -c 12-)
+if [[ "${GITHUB_REF}" =~ ^"refs/heads/" ]]; then
+  readonly BRANCH=$(echo "${GITHUB_REF}" | cut -c 12-)
   echo "branch: <https://github.com/${GITHUB_REPOSITORY}/tree/${BRANCH}|${BRANCH}>"
 else
-  readonly TAG=$(echo "${GITHUB_REPOSITORY_REF}" | cut -c 11-)
+  readonly TAG=$(echo "${GITHUB_REF}" | cut -c 11-)
   echo "tag: <https://github.com/${GITHUB_REPOSITORY}/releases/tag/${TAG}|${TAG}>"
 fi
 )
